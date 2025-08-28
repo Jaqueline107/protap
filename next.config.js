e007b2f0fb9d247812661a9adf37c849b1468947
Jaqueline Moura <Jaquelinemouraoliviera@gmail.com>
2025-08-28 20:02:14 -0300

    delete files

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['protapcars.vercel.app', 'firebasestorage.googleapis.com'], // adicione todos os domínios que você vai usar
-  },
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig;
