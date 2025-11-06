29527c12376504b9ae2e6859dbd427f9d498b197
Jaqueline Moura <Jaquelinemouraoliviera@gmail.com>
2025-11-06 13:11:22 -0300

    update ui bannr

--- src/app/Components/banner.tsx
+++ src/app/Components/banner.tsx
@@ -5,10 +5,12 @@ import CarpetSelector from "../CarpetSelector/page";
 
 export default function Banner() {
   return (
-    <section className="w-full md:h-auto h-80 bg-gradient-to-r from-black via-[#1C1C1C] to-black text-white">
+    <section className="w-full h-96 md:h-80 bg-gradient-to-r from-black via-[#1C1C1C] to-black text-white">
       <div className="max-w-6xl -mt-14 mx-auto px-6 lg:px-12 py-12 md:py-20 flex flex-col md:flex-row items-center justify-between gap-10">
+
         {/* Texto do Banner */}
         <div className="flex flex-col items-center md:items-start text-center md:text-left md:w-1/2">
+
           {/* Logo/Nome (oculto no mobile) */}
           <h1 className="hidden md:block text-3xl sm:text-5xl font-extrabold leading-tight drop-shadow-lg text-white">
             ProTap
@@ -34,12 +36,15 @@ export default function Banner() {
             </a>
           </div>
         </div>
-          <h1 className="md:hidden text-3xl -mt-10 sm:text-5xl font-bold leading-tight drop-shadow-lg text-white">
-           Tapetes Automotivos
-          </h1>
+
+        {/* Títulos Mobile */}
+        <h1 className="md:hidden text-3xl -mt-10 sm:text-5xl font-bold leading-tight drop-shadow-lg text-white">
+          Tapetes Automotivos
+        </h1>
         <p className="md:hidden -mt-10 -mb-3 text-[#B3B3B3] text-center">
           Melhor do Brasil
         </p>
+
         <CarpetSelector />
 
       </div>
